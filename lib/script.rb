10a08fb5848452404ee0c8a8b133d7282f960314
Vladimir <duenasvladimir@gmail.com>
2022-08-11 15:12:16 -0600

    Added a script that implements PathFinder module
    implementation of the PathFinder module on a knight piece

---
+++ lib/script.rb
@@ -0,0 +1,26 @@
+# !/usr/bin/env ruby
+# frozen_string_literal: false
+
+require_relative 'knight'
+require_relative 'board'
+
+board = ChessBoard.new(8)
+knight = Knight.new(false, 8)
+
+moves_counter = 0
+move_from_to = [[3, 3], [4, 3]]
+
+board.place_piece(knight, move_from_to[0])
+path = knight.find_path(*move_from_to)
+
+path.each do |square|
+  moves_counter += 1
+  knight.knight_moves(square)
+  board.update_board
+end
+
+board.pretty_print
+puts "\nYou made it in #{moves_counter - 1} moves! Here's your path:"
+path.each do |move|
+p move
+end
